2a3654a64044e5e580db1a31be3cfcfceb1ffb38
natmark <natmark0918@gmail.com>
2017-04-10 11:50:48 +0900

    :new: Add project

---
+++ NATagTextView/NATagTextView.h
@@ -0,0 +1,19 @@
+//
+//  NATagTextView.h
+//  NATagTextView
+//
+//  Created by AtsuyaSato on 2017/04/08.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NATagTextView.
+FOUNDATION_EXPORT double NATagTextViewVersionNumber;
+
+//! Project version string for NATagTextView.
+FOUNDATION_EXPORT const unsigned char NATagTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NATagTextView/PublicHeader.h>
+
+
